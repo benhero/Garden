3c6bedc5aabd198aa5e31a6d5576d110dbfadc8c
hero <xyzproject@163.com>
2019-05-16 14:53:15 +0800

    1. 整体支持Toolbar的点击控制页面跳转逻辑 2. 详情页支持滚动隐藏图片

--- app/src/main/java/com/ben/garden/GardenActivity.kt
+++ app/src/main/java/com/ben/garden/GardenActivity.kt
@@ -1,18 +1,34 @@
 package com.ben.garden
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class GardenActivity : AppCompatActivity() {
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.garden_activity)
-//        if (savedInstanceState == null) {
-//            supportFragmentManager.beginTransaction()
-//                .replace(R.id.container, GardenFragment.newInstance())
-//                .commitNow()
-//        }
+
+        // 获取导航控制器
+        navController = Navigation.findNavController(this, R.id.garden_nav_fragment)
+        // 关联导航控制器的导航图与其他组件
+        appBarConfiguration = AppBarConfiguration(navController.graph, null)
+        // 设置Toolbar
+        setSupportActionBar(findViewById(R.id.toolbar))
+        // 关联导航控制器与配置好的参数
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        // 处理导航器的点击
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
     }
 
 }
